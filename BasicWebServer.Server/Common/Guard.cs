03f2fe7424af23ccc38502d4cf11b176443456af
Daniel Stoimenov <daniel.stoimenov@itsoft.bg>
2022-04-19 11:28:58 +0300

    Add Guard.cs

---
+++ BasicWebServer.Server/Common/Guard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Common
+{
+    public static class Guard
+    {
+        public static void AgainstNull(object value, string name = null)
+        {
+            if (value == null)
+            {
+                name ??= "Value";
+                // name = name ?? "Value"; - name == name or name == "Value" if name is null
+
+                throw new ArgumentNullException($"{name} can not be null");
+            }
+        }
+    }
+}

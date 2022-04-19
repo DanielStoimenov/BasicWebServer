221f842dbd289371524c49d717d7880a9b41ea23
Daniel Stoimenov <daniel.stoimenov@itsoft.bg>
2022-04-19 09:51:17 +0300

    Add request and response

---
+++ BasicWebServer.Server/HTTP/Header.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicWebServer.Server.HTTP
+{
+    public class Header
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+        public Header(string _name, string _value)
+        {
+            Name = _name;
+            Value = _value;
+        }
+    }
+}
